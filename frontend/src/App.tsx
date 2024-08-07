a91aeec72f08a96e354426420089a4b7df726e58
lnardon <lucaslnardon@gmail.com>
2024-08-07 03:30:21 -0300

    ✨ Add files listing, frontend search and file retriaval

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,60 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div>Music Room</div>;
+  const [songs, setSongs] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("/getAllFiles")
+      .then((res) => res.json())
+      .then((data) => {
+        setSongs(data.files);
+        setSearchResults(data.files);
+        setLoading(false);
+      });
+  }, []);
+
+  useEffect(() => {
+    setSearchResults(
+      songs.filter((song: any) =>
+        song.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [search]);
+
+  function handleGetSong(song: any) {
+    return () => {
+      window.open(`/getSong?file=${song}`, "_blank");
+    };
+  }
+
+  return (
+    <div>
+      <h1>Music Player</h1>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <ul>
+          {searchResults.map((song: any, index: number) => (
+            <li key={index} onClick={handleGetSong(song)}>
+              {song}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default App;
