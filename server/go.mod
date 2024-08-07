0b9e94659ffba8b75349aa64d39f0137f7fa3554
lnardon <lucaslnardon@gmail.com>
2024-08-07 18:18:54 -0300

    ✨ Add endpoint to get all the artists found

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,8 @@
 module server
 
 go 1.22.5
+
+require (
+	github.com/bogem/id3v2 v1.2.0 // indirect
+	golang.org/x/text v0.3.2 // indirect
+)
--- server/main.go
+++ server/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/bogem/id3v2"
 )
 
 type FileList struct {
@@ -17,9 +20,12 @@ func main(){
     http.Handle("/", http.FileServer(http.Dir("../frontend/dist")))
 
     http.HandleFunc("/getAllFiles", GetAllFilesHandler)
+    http.HandleFunc("/getAllArtists", GetAllArtistsHandler)
     http.HandleFunc("/getSong", GetSongHandler)
 
+
     PORT := ":7777"
+
     fmt.Println("*")
     fmt.Println("**")
     fmt.Println("***")
@@ -27,6 +33,7 @@ func main(){
     fmt.Println("***")
     fmt.Println("**")
     fmt.Println("*")
+
     http.ListenAndServe(PORT, nil)
 }
 
@@ -39,6 +46,45 @@ func GetAllFilesHandler(w http.ResponseWriter, r *http.Request){
 
     fileList := FileList{Files: files}
     jsonResponse, err := json.Marshal(fileList)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.Write(jsonResponse)
+}
+
+func GetAllArtistsHandler(w http.ResponseWriter, r *http.Request){
+    files, err := getAllFilesInPath("/media/lucas/HDD1/Music/")
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    artists := make(map[string]bool)
+    for _, file := range files {
+        tag, err := id3v2.Open(file, id3v2.Options{Parse: true})
+        if err != nil {
+            log.Println("Error opening MP3 file:", err)
+            continue
+        }
+        defer tag.Close()
+
+        allSongsArtists := strings.Split(tag.Artist(), "/")
+        for _, artist := range allSongsArtists {
+            if artist != "" {
+                artists[artist] = true
+            }
+        }
+    }
+
+    var artistList []string
+    for artist := range artists {
+        artistList = append(artistList, artist)
+    }
+
+    jsonResponse, err := json.Marshal(artistList)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
