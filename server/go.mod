fad17303f73559572768599884f1d45d132da3f0
lnardon <lucaslnardon@gmail.com>
2024-08-13 06:28:00 -0300

    ✨ Add uuid package + db population handler

--- server/go.mod
+++ server/go.mod
@@ -4,6 +4,7 @@ go 1.22.5
 
 require (
 	github.com/bogem/id3v2 v1.2.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	golang.org/x/text v0.3.2 // indirect
 )
--- server/utils/organization.go
+++ server/utils/organization.go
@@ -1,11 +1,70 @@
 package utils
 
 import (
+	"database/sql"
 	"log"
 	"strings"
 
 	"github.com/bogem/id3v2"
+	"github.com/google/uuid"
 )
+
+func PopulateDb(path string) {
+	db, err := sql.Open("sqlite3", "./db/database.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	files, err := GetAllFilesInPath(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		tag, err := id3v2.Open(file, id3v2.Options{Parse: true})
+		if err != nil {
+			log.Println("Error opening MP3 file:", err)
+			continue
+		}
+		defer tag.Close()
+
+		artistID, albumID := "", ""
+		artistName := strings.Split(tag.Artist(), "/")[0]
+		albumTitle := tag.Album()
+		songTitle := tag.Title()
+		duration := "3:30" // Fix this
+		releaseDate := tag.Year()
+
+		err = db.QueryRow("SELECT id FROM Artists WHERE name = ?", artistName).Scan(&artistID)
+		if err == sql.ErrNoRows {
+			artistID = uuid.New().String()
+			_, err = db.Exec("INSERT INTO Artists (id, name, avatar, albums) VALUES (?, ?, '', '')", artistID, artistName)
+			if err != nil {
+				log.Println("Error inserting artist:", err)
+				continue
+			}
+		}
+
+		err = db.QueryRow("SELECT id FROM Albums WHERE title = ? AND artist = ?", albumTitle, artistID).Scan(&albumID)
+		if err == sql.ErrNoRows {
+			albumID = uuid.New().String()
+			_, err = db.Exec("INSERT INTO Albums (id, title, cover, release_date, artist) VALUES (?, ?, '', ?, ?)", albumID, albumTitle, releaseDate, artistID)
+			if err != nil {
+				log.Println("Error inserting album:", err)
+				continue
+			}
+		}
+
+		songID := uuid.New().String()
+		_, err = db.Exec("INSERT INTO Songs (id, title, duration, track_number, release_date, path, album, lyrics) VALUES (?, ?, ?, '0', ?, ?, ?, 'N/A')", songID, songTitle, duration, releaseDate, file, albumID)
+		if err != nil {
+			log.Println("Error inserting song:", err)
+			continue
+		}
+	}
+}
+
 
 // This function updates the artists string in the MP3 files metadata to use a ; as separator instead of a /
 // Example: "Artist1/Artist2" -> "Artist1;Artist2"
@@ -30,4 +89,4 @@ func UpdateArtistsString(path string) {
 		}
 	}
 	
-} 
+}
