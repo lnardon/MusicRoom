caefce2ef5f2d58c05e17f7ca8ce416f3820d6a4
lnardon <lucaslnardon@gmail.com>
2024-08-13 01:28:00 -0300

    ✨ Add sqlite support

--- server/main.go
+++ server/main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 	"strings"
 
+	Utils "server/utils"
+
 	"github.com/bogem/id3v2"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type FileList struct {
@@ -44,7 +47,7 @@ func main(){
 }
 
 func GetAllFilesHandler(w http.ResponseWriter, r *http.Request){
-    files, err := getAllFilesInPath("/media/lucas/HDD1/Music/")
+    files, err := Utils.GetAllFilesInPath("/media/lucas/HDD1/Music/")
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
@@ -62,46 +65,42 @@ func GetAllFilesHandler(w http.ResponseWriter, r *http.Request){
 }
 
 func GetAllArtistsHandler(w http.ResponseWriter, r *http.Request){
-    files, err := getAllFilesInPath("/media/lucas/HDD1/Music/")
+    db, err := sql.Open("sqlite3", "./db/database.db")
+	if err != nil {
+		http.Error(w, "Failed to open database", http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
+
+    rows, err := db.Query("SELECT * FROM Artists")
+    if err != nil {
+        http.Error(w, "Failed to query database", http.StatusInternalServerError)
+        return
+    }
+    defer rows.Close()
+
+    var artists []string
+    for rows.Next() {
+        var artist string
+        if err := rows.Scan(&artist); err != nil {
+            http.Error(w, "Failed to scan row", http.StatusInternalServerError)
+            return
+        }
+        artists = append(artists, artist)
+    }
+
+    jsonResponse, err := json.Marshal(artists)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
-
-    artists := make(map[string]bool)
-    for _, file := range files {
-        tag, err := id3v2.Open(file, id3v2.Options{Parse: true})
-        if err != nil {
-            log.Println("Error opening MP3 file:", err)
-            continue
-        }
-        defer tag.Close()
-
-        allSongsArtists := strings.Split(tag.Artist(), "/")
-        for _, artist := range allSongsArtists {
-            if artist != "" {
-                artists[artist] = true
-            }
-        }
-    }
-
-    var artistList []string
-    for artist := range artists {
-        artistList = append(artistList, artist)
-    }
-
-    jsonResponse, err := json.Marshal(artistList)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-
+    
     w.Header().Set("Content-Type", "application/json")
     w.Write(jsonResponse)
 }
 
 func GetAllAlbumsHandler(w http.ResponseWriter, r *http.Request){
-    files, err := getAllFilesInPath("/media/lucas/HDD1/Music/")
+    files, err := Utils.GetAllFilesInPath("/media/lucas/HDD1/Music/")
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
@@ -137,20 +136,6 @@ func GetAllAlbumsHandler(w http.ResponseWriter, r *http.Request){
     w.Write(jsonResponse)
 }
 
-func getAllFilesInPath(path string) ([]string, error) {
-	var files []string
-	err := filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		if !info.IsDir() && strings.HasSuffix(path, ".mp3") {
-			files = append(files, path)
-		}
-		return nil
-	})
-	return files, err
-}
-
 func GetSongHandler(w http.ResponseWriter, r *http.Request){
     file := r.URL.Query().Get("file")
     
@@ -179,7 +164,7 @@ func GetCoverHandler(w http.ResponseWriter, r *http.Request){
 }
 
 func GetCover(path string) string{
-	files, err := getAllFilesInPath(path)
+	files, err := Utils.GetAllFilesInPath(path)
 	if err != nil {
 		log.Fatalf("Error fetching files: %v", err)
 	}
@@ -217,7 +202,7 @@ type Song struct {
 func GetAlbumHandler(w http.ResponseWriter, r *http.Request){
     album := r.URL.Query().Get("album")
 
-    files, err := getAllFilesInPath("/media/lucas/HDD1/Music/")
+    files, err := Utils.GetAllFilesInPath("/media/lucas/HDD1/Music/")
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
@@ -261,7 +246,7 @@ type ArtistInfo struct {
 func GetArtistHandler(w http.ResponseWriter, r *http.Request) {
     artist := r.URL.Query().Get("artist")
 
-    files, err := getAllFilesInPath(fmt.Sprint("/media/lucas/HDD1/Music/", artist))
+    files, err := Utils.GetAllFilesInPath(fmt.Sprint("/media/lucas/HDD1/Music/", artist))
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
