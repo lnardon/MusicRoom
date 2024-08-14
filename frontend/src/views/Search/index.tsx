2a3ee4bbe4a6bc0b7a2d0f4f2682b258865c5fe0
lnardon <lucaslnardon@gmail.com>
2024-08-14 02:54:59 -0300

    ♻️ Refactor search view

---
+++ frontend/src/views/Search/index.tsx
@@ -0,0 +1,130 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useState } from "react";
+import { usePlayerStore } from "../../stores/playerStore";
+import { useUrlStore } from "../../stores/urlStore";
+
+import styles from "./styles.module.css";
+
+const Home = () => {
+  const [songs, setSongs] = useState([]);
+  const [filteredSongs, setFilteredSongs] = useState([]);
+
+  const [artists, setArtists] = useState([]);
+  const [filteredArtists, setFilteredArtists] = useState([]);
+
+  const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("/getAllFiles")
+      .then((res) => res.json())
+      .then((data) => {
+        setSongs(data.files);
+        setFilteredSongs(data.files);
+        setLoading(false);
+      });
+
+    fetch("/getAllArtists")
+      .then((res) => res.json())
+      .then((data) => {
+        setArtists(data);
+        setFilteredArtists(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    setFilteredSongs(
+      songs.filter((song: any) =>
+        song.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+
+    setFilteredArtists(
+      artists.filter((artist: any) =>
+        artist.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [search]);
+
+  function handleGetSong(song: any) {
+    usePlayerStore.setState({
+      filePath: `/getSong?file=${song}`,
+      title: song.split("/")[song.split("/").length - 1],
+      artist: song.split("/")[song.split("/").length - 3],
+      cover: `/getCover?file=${song}`,
+      isPlaying: true,
+    });
+  }
+
+  function handleOpenArtistProfile(artist: any) {
+    const searchParams = new URLSearchParams();
+    searchParams.set("artist", artist);
+    searchParams.set("view", "artist_profile");
+    window.history.pushState(
+      {},
+      "",
+      `${window.location.pathname}?${searchParams.toString()}`
+    );
+    useUrlStore.getState().setUrl(window.location.search);
+  }
+
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Search</h1>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        className={styles.input}
+        placeholder="Search for songs, artists, and albums..."
+      />
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          <div className={styles.artistsContainer}>
+            {filteredArtists.map((artist: any, index: number) => (
+              <div
+                className={styles.artist}
+                key={index}
+                onClick={() => handleOpenArtistProfile(artist)}
+              >
+                <img
+                  src="https://st3.depositphotos.com/9998432/13335/v/450/depositphotos_133351928-stock-illustration-default-placeholder-man-and-woman.jpg"
+                  alt=""
+                  className={styles.avatar}
+                />
+                <p>{artist}</p>
+              </div>
+            ))}
+          </div>
+          <ul
+            style={{
+              width: "100%",
+            }}
+          >
+            {filteredSongs.map((song: any, index: number) => (
+              <div
+                style={{
+                  cursor: "pointer",
+                  textAlign: "start",
+                  marginBottom: "2rem",
+                }}
+                key={index}
+                onClick={() => handleGetSong(song)}
+              >
+                <div>{"| " + song.split("/").slice(5, 6)}</div>
+                <div>{"|---- " + song.split("/").slice(6, 7)}</div>
+                <div>{"|-------- " + song.split("/").slice(7, 8)}</div>
+              </div>
+            ))}
+          </ul>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Home;
