b7aaae535d49e590cb8d32dced2be570ed074821
lnardon <lucaslnardon@gmail.com>
2024-08-15 00:11:55 -0300

    💄 Update Home ui

--- frontend/src/views/Home/index.tsx
+++ frontend/src/views/Home/index.tsx
@@ -2,15 +2,22 @@
 import { useState, useEffect } from "react";
 
 import { usePlayerStore } from "../../stores/playerStore";
+import { useUrlStore } from "../../stores/urlStore";
+
+import styles from "./styles.module.css";
+import AnimatedText from "animated-text-letters";
 
 const Home: React.FC = () => {
   const [recentlyPlayed, setRecentlyPlayed] = useState([]);
+  const [lastAlbums, setLastAlbums] = useState([]);
 
   function handleClick(track: any) {
-    usePlayerStore.getState().setFilePath(`/getSong?file=${track.id}`);
-    usePlayerStore.getState().setTitle(track.title);
-    usePlayerStore.getState().setArtist(track.artist);
-    usePlayerStore.getState().setCover(`/getCover?file=${track.album}`);
+    usePlayerStore.getState().setSong({
+      title: track.title,
+      artist: track.artist,
+      cover: `/getCover?file=${track.album}`,
+      file: `/getSong?file=${track.id}`,
+    });
     usePlayerStore.getState().setIsPlaying(true);
   }
 
@@ -18,7 +25,8 @@ const Home: React.FC = () => {
     fetch("/getHistory")
       .then((res) => res.json())
       .then((data) => {
-        setRecentlyPlayed(data);
+        setRecentlyPlayed(data.songs);
+        setLastAlbums(data.albums.slice(0, 8));
       });
   }, []);
 
@@ -30,30 +38,107 @@ const Home: React.FC = () => {
         height: "calc(100vh - 6rem)",
       }}
     >
+      <h1 className={styles.title}>
+        <AnimatedText
+          text="Home"
+          animation="fade-in"
+          delay={64}
+          easing="ease"
+          transitionOnlyDifferentLetters={true}
+          animationDuration={800}
+        />
+      </h1>
+      <div
+        style={{
+          marginBottom: "1rem",
+        }}
+      >
+        <h3
+          style={{
+            textAlign: "start",
+            color: "white",
+            fontSize: "1rem",
+            fontWeight: "600",
+            padding: "1rem 2rem 0rem 2rem",
+          }}
+        >
+          Last Albums:
+        </h3>
+
+        <div className={styles.albums}>
+          {lastAlbums.map((album: any, index: number) => (
+            <div
+              className={styles.album}
+              key={album}
+              style={{
+                animationDelay: `${index * 0.08}s`,
+              }}
+              onClick={() => {
+                const searchParams = new URLSearchParams(
+                  window.location.search
+                );
+                searchParams.set("album", album);
+                searchParams.set("view", "album");
+                window.history.pushState(
+                  {},
+                  "",
+                  `${window.location.pathname}?${searchParams.toString()}`
+                );
+                useUrlStore.getState().setUrl(window.location.search);
+              }}
+            >
+              <img
+                src={`/getCover?file=${album}`}
+                alt=""
+                style={{
+                  width: "100%",
+                  borderRadius: "0.5rem",
+                  marginBottom: "0.5rem",
+                }}
+              />
+              <p
+                style={{
+                  textAlign: "left",
+                  fontWeight: "600",
+                }}
+              >
+                {album}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <h4
+        style={{
+          textAlign: "start",
+          color: "white",
+          fontSize: "1rem",
+          fontWeight: "600",
+          padding: "1rem 2rem 0rem 2rem",
+        }}
+      >
+        Recently Played:
+      </h4>
       <div
         style={{
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
           flexDirection: "column",
-          padding: "20px",
+          padding: "1rem 2rem",
           width: "100%",
         }}
       >
-        {recentlyPlayed?.map((track: any) => (
+        {recentlyPlayed?.map((track: any, index: number) => (
           <div
             key={track}
             style={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "flex-start",
-              alignItems: "center",
-              width: "100%",
-              marginBottom: "1rem",
-              fontSize: "1rem",
-              fontWeight: "400",
-              cursor: "pointer",
+              backgroundColor:
+                index % 2 === 0
+                  ? "rgba(23, 23, 23, 1)"
+                  : "rgba(23, 23, 23, 0.44)",
             }}
+            className={styles.track}
             onClick={() => handleClick(track)}
           >
             <img
