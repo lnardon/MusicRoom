10fa95bc6b4ebbcdab625f4f38e1c1b8efac586f
lnardon <lucaslnardon@gmail.com>
2024-08-16 02:55:57 -0300

    💄 Add current view indicator to sidebar

--- frontend/src/Components/Sidebar/index.tsx
+++ frontend/src/Components/Sidebar/index.tsx
@@ -2,8 +2,10 @@ import { Home, Search, Settings, BarChart2, ListMusicIcon } from "lucide-react";
 import styles from "./styles.module.css";
 
 const Sidebar = ({
+  currentView,
   setCurrentView,
 }: {
+  currentView: string;
   setCurrentView: (view: string) => void;
 }) => {
   return (
@@ -16,14 +18,31 @@ const Sidebar = ({
         MusiCO
       </h1>
       <div className={styles.button} onClick={() => setCurrentView("home")}>
-        <Home size={22} />
-        <span className={styles.text} style={{ fontSize: "1rem" }}>
+        <Home size={22} color={currentView === "home" ? "#ffd000" : "white"} />
+        <span
+          className={styles.text}
+          style={{
+            fontSize: "1rem",
+            fontWeight: currentView === "home" ? "bold" : 400,
+            color: currentView === "home" ? "#ffd000" : "white",
+          }}
+        >
           Home
         </span>
       </div>
       <div className={styles.button} onClick={() => setCurrentView("search")}>
-        <Search size={22} />
-        <span className={styles.text} style={{ fontSize: "1rem" }}>
+        <Search
+          size={22}
+          color={currentView === "search" ? "#ffd000" : "white"}
+        />
+        <span
+          className={styles.text}
+          style={{
+            fontSize: "1rem",
+            fontWeight: currentView === "search" ? "bold" : 400,
+            color: currentView === "search" ? "#ffd000" : "white",
+          }}
+        >
           Search
         </span>
       </div>
@@ -33,19 +52,34 @@ const Sidebar = ({
           opacity: 0.3,
         }}
       >
-        <ListMusicIcon size={22} />
-        <span className={styles.text} style={{ fontSize: "1rem" }}>
+        <ListMusicIcon
+          size={22}
+          color={currentView === "playlist" ? "#ffd000" : "white"}
+        />
+        <span
+          className={styles.text}
+          style={{
+            fontSize: "1rem",
+            fontWeight: currentView === "playlist" ? "bold" : 400,
+            color: currentView === "playlist" ? "#ffd000" : "white",
+          }}
+        >
           Playlist
         </span>
       </div>
-      <div
-        className={styles.button}
-        style={{
-          opacity: 0.3,
-        }}
-      >
-        <BarChart2 size={22} />
-        <span className={styles.text} style={{ fontSize: "1rem" }}>
+      <div className={styles.button} onClick={() => setCurrentView("stats")}>
+        <BarChart2
+          size={22}
+          color={currentView === "stats" ? "#ffd000" : "white"}
+        />
+        <span
+          className={styles.text}
+          style={{
+            fontSize: "1rem",
+            fontWeight: currentView === "stats" ? "bold" : 400,
+            color: currentView === "stats" ? "#ffd000" : "white",
+          }}
+        >
           Stats
         </span>
       </div>
@@ -55,8 +89,18 @@ const Sidebar = ({
           opacity: 0.3,
         }}
       >
-        <Settings size={22} />
-        <span className={styles.text} style={{ fontSize: "1rem" }}>
+        <Settings
+          size={22}
+          color={currentView === "settings" ? "#ffd000" : "white"}
+        />
+        <span
+          className={styles.text}
+          style={{
+            fontSize: "1rem",
+            fontWeight: currentView === "settings" ? "bold" : 400,
+            color: currentView === "settings" ? "#ffd000" : "white",
+          }}
+        >
           Settings
         </span>
       </div>
