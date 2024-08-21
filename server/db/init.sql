240f82f31ae3400c17ede25b0da363e4ead10118
lnardon <lucaslnardon@gmail.com>
2024-08-21 04:51:28 -0300

    🗃️ Update db schema to add playlists

--- server/db/init.sql
+++ server/db/init.sql
@@ -1,33 +1,43 @@
-CREATE TABLE "Artists" (
-    "id" CHAR(36) NOT NULL,
-    "name" TEXT NOT NULL,
-    "albums" CHAR(36) NOT NULL,
-    "avatar" TEXT NOT NULL,
-    PRIMARY KEY("id")
+CREATE TABLE `Artists`(
+    `id` CHAR(36) NOT NULL,
+    `name` TEXT NOT NULL,
+    `albums` CHAR(36) NULL,
+    `avatar` TEXT NULL,
+    PRIMARY KEY(`id`)
 );
-
-CREATE TABLE "Albums" (
-    "id" CHAR(36) NOT NULL,
-    "title" TEXT NOT NULL,
-    "cover" TEXT NOT NULL,
-    "release_date" TEXT NOT NULL,
-    "songs" CHAR(36) NOT NULL,
-    PRIMARY KEY("id")
+CREATE TABLE `Albums`(
+    `id` CHAR(36) NOT NULL,
+    `title` TEXT NOT NULL,
+    `cover` TEXT NULL,
+    `release_date` TEXT NULL,
+    `artist` CHAR(36) NOT NULL,
+    FOREIGN KEY(`artist`) REFERENCES `Artists`(`id`),
+    PRIMARY KEY(`id`)
 );
-
-CREATE TABLE "Songs" (
-    "id" CHAR(36) NOT NULL,
-    "title" TEXT NOT NULL,
-    "duration" TEXT NOT NULL,
-    "track_number" INT NOT NULL,
-    "release_date" TEXT NOT NULL,
-    "path" TEXT NOT NULL,
-    "lyrics" TEXT NOT NULL,
-    PRIMARY KEY("id")
+CREATE TABLE `Songs`(
+    `id` CHAR(36) NOT NULL,
+    `title` TEXT NOT NULL,
+    `duration` TEXT NULL,
+    `track_number` INT NULL,
+    `release_date` TEXT NULL,
+    `path` TEXT NOT NULL,
+    `lyrics` TEXT NULL,
+    `album` CHAR(36) NULL,
+    FOREIGN KEY(`album`) REFERENCES `Albums`(`id`),
+    PRIMARY KEY(`id`)
 );
-
-ALTER TABLE "Artists"
-    ADD CONSTRAINT "artists_albums_fk" FOREIGN KEY("albums") REFERENCES "Albums"("id");
-
-ALTER TABLE "Albums"
-    ADD CONSTRAINT "albums_songs_fk" FOREIGN KEY("songs") REFERENCES "Songs"("id");
+CREATE TABLE `History`(
+    `id` CHAR(36) NOT NULL,
+    `song` CHAR(36) NOT NULL,
+    `timestamp` TIMESTAMP NOT NULL,
+    FOREIGN KEY(`song`) REFERENCES `Songs`(`id`),
+    PRIMARY KEY(`id`)
+);
+CREATE TABLE `Playlists`(
+    `id` CHAR(36) NOT NULL,
+    `name` TEXT NOT NULL,
+    `cover` TEXT NOT NULL,
+    `songs` CHAR(36) NOT NULL,
+    FOREIGN KEY(`songs`) REFERENCES `Songs`(`id`),
+    PRIMARY KEY(`id`)
+);
