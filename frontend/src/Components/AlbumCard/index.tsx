4006c8da180b320f4348b3de04d23243b1ae40b0
Lucas Nardon <lucaslnardon@gmail.com>
2025-01-10 23:23:22 -0300

    Create AlbumCard component

---
+++ frontend/src/Components/AlbumCard/index.tsx
@@ -0,0 +1,34 @@
+import { Album } from "../../types";
+import { HandleFallbackImage } from "../../utils/helpers";
+import styles from "./styles.module.css";
+
+const AlbumCard = ({
+  album,
+  onClick,
+  index,
+}: {
+  album: Album;
+  onClick: () => void;
+  index: number;
+}) => {
+  return (
+    <div
+      className={styles.container}
+      key={album.id}
+      style={{
+        animationDelay: `${index * 0.08}s`,
+      }}
+      onClick={onClick}
+    >
+      {HandleFallbackImage(`/getCover?file=${album.id}`, "", null, {
+        width: "100%",
+        height: "max-content",
+        borderRadius: "0.5rem",
+        marginBottom: "0.5rem",
+      })}
+      <p className={styles.title}>{album.title}</p>
+    </div>
+  );
+};
+
+export default AlbumCard;
