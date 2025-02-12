41cd18707cc0f13166a4f1719edcf8273359f331
Lucas Nardon <lucaslnardon@gmail.com>
2025-02-11 23:07:19 -0300

    Refactor component to fetch image on private endpoint

--- frontend/src/utils/helpers.tsx
+++ frontend/src/utils/helpers.tsx
@@ -1,42 +1,44 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useState } from "react";
+
 export function HandleFallbackImage(
   url: string,
   className: string,
   ref?: any,
   styles?: any
 ): any {
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      try {
+        const token = localStorage.getItem("token");
+        const response = await fetch(url, {
+          headers: {
+            Authorization: `${token}`,
+          },
+        });
+
+        if (!response.ok) throw new Error("Failed to load image");
+
+        const blob = await response.blob();
+        setImageSrc(URL.createObjectURL(blob));
+      } catch (error) {
+        console.error(error);
+        setImageSrc(null);
+      }
+    };
+
+    if (url) fetchImage();
+  }, [url]);
+
   return (
-    <object
-      type="image/jpeg"
-      data={url}
-      aria-label="This image should exist, but alas it does not"
+    <img
+      src={imageSrc || "/assets/vinyl_placeholder.webp"}
+      alt="cover image"
       className={className}
       ref={ref}
       style={styles}
-    >
-      <img
-        src="/assets/vinyl_placeholder.webp"
-        alt="cover image"
-        style={{
-          width: "100%",
-          height: "100%",
-        }}
-      />
-    </object>
+    />
   );
-}
-
-export function urlHistoryHandler(
-  key: string,
-  value: string,
-  setUrl: (url: string) => void
-) {
-  const searchParams = new URLSearchParams(window.location.search);
-  searchParams.set(key, searchParams.get(key) === value ? "" : value);
-  window.history.pushState(
-    {},
-    "",
-    `${window.location.pathname}?${searchParams.toString()}`
-  );
-  setUrl(window.location.search);
 }
